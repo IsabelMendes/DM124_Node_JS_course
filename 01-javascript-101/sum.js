7706226e0cba89dffda09d8cf9af8bee55d73f3b
IsabelMendes <isabelfrancinem@gmail.com>
2019-08-03 15:32:45 -0300

    nova forma de somar varios numeros

--- 01-javascript-101/sum.js
+++ 01-javascript-101/sum.js
@@ -1,6 +1,18 @@
-let numberA = 11;
-let numberB = 31;
+const values = process.argv.splice(2);
+console.log(values);
 
-const total = numberA + numberB;
-console.log('The sum is: ' + total);
+const convertToNumber = value => parseInt(value);
+const numbers = values.map(convertToNumber);
+console.log(numbers);
+
+const sum = (total, current) => total + current;
+
+const total = numbers.reduce(sum);
+console.log(total)
+
+/*const total = values
+    .map(convertToNumber)
+    .reduce(sum);*/
+
+console.log('The sum is: ' + total);    
 
