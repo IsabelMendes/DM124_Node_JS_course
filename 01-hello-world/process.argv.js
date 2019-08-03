ea58a1747df0f2fc2f1cbdfa7115f3c76f7c71a4
IsabelMendes <isabelfrancinem@gmail.com>
2019-08-03 16:27:09 -0300

    ordenar nomes

--- 01-hello-world/process.argv.js
+++ 01-hello-world/process.argv.js
@@ -1,1 +1,14 @@
-console.log(process.argv);
+const values = process.argv.slice(2);
+
+//values.forEach(values => console.log ('The values is:' + values))
+
+function regularPrint(values){
+    console.log('The values is: ' + values);
+}
+function fancyPrint(values) {
+console.log(`---> ${values} <--`);
+}
+
+//const print = fancyPrint;
+const print = eval(process.env.PRINTER) || regularPrint;
+values.forEach(print);
---
+++ 01-javascript-101/javascript-funcional.js
@@ -0,0 +1,12 @@
+const names = [
+    'Doralice',
+    'Hanna',
+    'Clotilde'
+];
+
+const print =name => console.log(name);
+
+names
+.sort()
+.reverse()
+.forEach(print);
