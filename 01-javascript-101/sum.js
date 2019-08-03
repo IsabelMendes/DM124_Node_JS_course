70852a5ec559d021dd84ff8ac1f6ac4e9aaefbb1
IsabelMendes <isabelfrancinem@gmail.com>
2019-08-03 15:44:15 -0300

    outra forma

--- 01-javascript-101/sum.js
+++ 01-javascript-101/sum.js
@@ -1,11 +1,11 @@
-const values = process.argv.splice(2);
+const values = process.argv.splice(2);//sim
 console.log(values);
 
-const convertToNumber = value => parseInt(value);
+const convertToNumber = value => parseInt(value);//sim
 const numbers = values.map(convertToNumber);
 console.log(numbers);
 
-const sum = (total, current) => total + current;
+const sum = (total, current) => total + current;//sim
 
 const total = numbers.reduce(sum);
 console.log(total)
